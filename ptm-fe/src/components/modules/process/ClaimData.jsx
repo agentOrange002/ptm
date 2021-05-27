e9df60950bcd66f262d1b1bfe6fa127a6864cdb5
agentOrange002 <nehemiasbelong@gmail.com>
2021-05-28 01:01:45 +0800

    convert gradle project into maven

--- ptm-fe/src/components/modules/process/ClaimData.jsx
+++ ptm-fe/src/components/modules/process/ClaimData.jsx
@@ -1,10 +1,108 @@
 import React, { Component } from 'react';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { getAllClaims } from '../../../redux/actions/ClaimActions';
+import UILoader from '../tools/UILoader';
+import history from '../../../routes/history';
+import { ContextMenu } from 'primereact/contextmenu';
+
+const MyStyle = {
+	id: { width: '150px' },
+	claimId: { width: '250px' },
+	recruiter: { width: '750px' },
+};
 
 class ClaimData extends Component {
-	state = {};
+	state = {
+		claims: null,
+		selectedClaim: null,
+		menu: [
+			{
+				label: 'View Claim',
+				icon: 'pi pi-fw pi-search',
+				command: (event) => this.viewClaim(this.state.selectedClaim),
+			},
+		],
+	};
+
+	async componentDidMount() {
+		if (_.isEmpty(this.props.CLAIMS)) await this.props.getAllClaims();
+	}
+
+	viewClaim(claim) {
+		let id = claim.claimId;
+		history.push(`/app/process/claim/view/${id}`);
+	}
+
+	hideContext = () => {
+		this.setState({ selectedClaim: null });
+	};
+
+	refreshTable = async (event) => {
+		event.preventDefault();
+		await this.props.getAllClaims();
+	};
+
+	displaySelection(data) {
+		if (!data || data.length === 0) {
+			return <div style={{ textAlign: 'left' }}>No Selection</div>;
+		} else {
+			if (data instanceof Array)
+				return (
+					<ul style={{ textAlign: 'left', margin: 0 }}>
+						{data.map((claim, i) => (
+							<li key={claim.claimId}>{claim.claimId + ' - ' + claim.id + ' - ' + claim.memberClaimDetails.fullName}</li>
+						))}
+					</ul>
+				);
+			else return <div style={{ textAlign: 'left' }}>Selected Claim: {data.claimId + ' - ' + data.id + ' - ' + data.memberClaimDetails.fullName}</div>;
+		}
+	}
+
 	render() {
-		return <div></div>;
+		const paginatorLeft = <Button icon='pi pi-refresh' onClick={this.refreshTable} />;
+		return (
+			<div className='content-section implementation'>
+				<ContextMenu model={this.state.menu} ref={(el) => (this.cm = el)} onHide={this.hideContext} />
+				<UILoader blockui='CLAIM_LOADING' unblockui={['CLAIM_GET_ALL', 'CLAIM_ERROR']}>
+					<DataTable
+						value={this.props.CLAIMS}
+						sortField='id'
+						sortOrder={-1}
+						scrollable={true}
+						selectionMode='single'
+						header='Claim Data Table'
+						footer={this.displaySelection(this.state.selectedClaim)}
+						selection={this.state.selectedClaim}
+						onSelectionChange={(e) => this.setState({ selectedClaim: e.value })}
+						paginator={true}
+						paginatorLeft={paginatorLeft}
+						paginatorTemplate='FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown'
+						currentPageReportTemplate='Showing {first} to {last} of {totalRecords} entries'
+						rows={10}
+						rowsPerPageOptions={[5, 10, 20]}
+						contextMenuSelection={this.state.selectedClaim}
+						onContextMenuSelectionChange={(e) => this.setState({ selectedClaim: e.value })}
+						onContextMenu={(e) => this.cm.show(e.originalEvent)}>
+						<Column field='id' header='ID' style={MyStyle.id} />
+						<Column field='claimId' header='Claim ID' style={MyStyle.claimId} />
+						<Column field='memberClaimDetails.fullName' header='Recruiter' style={MyStyle.recruiter} />
+					</DataTable>
+				</UILoader>
+			</div>
+		);
 	}
 }
 
-export default ClaimData;
+const mapStateToProps = (state) => {
+	return {
+		CLAIMS: Object.values(state.CLAIMS.claimsResponse),
+	};
+};
+
+const mapDispatchToProps = { getAllClaims };
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClaimData);
--- ptm-fe/src/components/modules/process/ClaimForm.jsx
+++ ptm-fe/src/components/modules/process/ClaimForm.jsx
@@ -1,10 +1,108 @@
 import React, { Component } from 'react';
+import { Panel } from 'primereact/panel';
+import { connect } from 'react-redux';
+import UILoader from '../tools/UILoader';
+import { saveClaim } from '../../../redux/actions/ClaimActions';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { Messages } from 'primereact/messages';
+import { CLAIM_SAVE } from '../../../redux/constants/ClaimConstants';
+import _ from 'lodash';
+import { Chips } from 'primereact/chips';
+
+const MyStyle = {
+	ButtonStyle: { paddingTop: '10px', paddingBottom: '35px' },
+	Button: { marginRight: '.25em', float: 'right', width: '150px' },
+	top: { paddingTop: '20px' },
+	ttop: { position: 'top' },
+	twidth: { width: '100%' },
+	divPaddingTop: { paddingTop: '30px' },
+	lable: { marginLeft: '10px' },
+	divMargin: { marginLeft: '20px' },
+};
 
 class ClaimForm extends Component {
-	state = {};
+	state = {
+		memberId: '',
+		members: [],
+	};
+
+	showError(message) {
+		let msg = message;
+		if (_.isEmpty(message)) msg = 'Error Claim Registration';
+		this.messages.show({
+			sticky: true,
+			severity: 'error',
+			summary: 'Error Message :',
+			detail: msg,
+		});
+	}
+
+	showSuccess() {
+		this.messages.show({
+			sticky: true,
+			severity: 'success',
+			summary: 'Success Message :',
+			detail: 'Successfully Save Claim!',
+		});
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (this.props.ERROR_MESSAGE !== prevProps.ERROR_MESSAGE) {
+			if (this.props.ERROR) {
+				this.showError(this.props.ERROR_MESSAGE.message);
+			}
+		}
+		if (this.props.CLAIMS !== prevProps.CLAIMS) {
+			if (this.props.FETCHTYPE === CLAIM_SAVE) {
+				this.setState({ memberId: '' });
+				this.setState({ members: [] });
+				this.showSuccess();
+			}
+		}
+	}
+
+	onSave = async (event) => {
+		event.preventDefault();
+		await this.props.saveClaim(this.state.memberId, { members: this.state.members });
+	};
+
 	render() {
-		return <div></div>;
+		return (
+			<UILoader blockui='CLAIM_LOADING' unblockui={['CLAIM_ERROR', 'CLAIM_SAVE']}>
+				<Messages ref={(el) => (this.messages = el)}></Messages>
+				<Panel header={this.props.title}>
+					<div className='p-grid p-fluid'>
+						<div className='p-col-12 p-md-12' style={MyStyle.divPaddingTop}>
+							<span className='p-float-label'>
+								<InputText in='recruiter' tooltip='EnterRecruiter' tooltipOptions={MyStyle.ttop} style={MyStyle.twidth} value={this.state.memberId} onChange={(e) => this.setState({ memberId: e.target.value })} />
+								<label htmlFor='recruiter'>Enter Recruiter</label>
+							</span>
+						</div>
+						<div className='p-col-12 p-md-12' style={MyStyle.divPaddingTop}>
+							<Chips value={this.state.members} onChange={(e) => this.setState({ members: e.value })} separator=',' />
+						</div>
+					</div>
+					<div className='button' style={MyStyle.ButtonStyle}>
+						<span>
+							<Button disabled={_.isEmpty(this.state.memberId || this.state.members)} icon='pi pi-save' label='Save' style={MyStyle.Button} onClick={this.onSave} />
+						</span>
+					</div>
+				</Panel>
+			</UILoader>
+		);
 	}
 }
 
-export default ClaimForm;
+const mapStateToProps = (state) => {
+	return {
+		CLAIMS: state.CLAIMS.claimsResponse,
+		ERROR: state.CLAIMS.fetchError,
+		ERROR_MESSAGE: state.CLAIMS.fetchErrorMessage,
+		FETCHTYPE: state.CLAIMS.fetchType,
+	};
+};
+
+const mapDispatchToProps = { saveClaim };
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClaimForm);
